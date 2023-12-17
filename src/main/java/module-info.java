9b5e0e0de9ae3a7febf636019d1fa2a9bc8ac671
Zejd Krek <zejd.krek@gmail.com>
2023-12-17 18:21:02 +0100

    add missing require for jackson objectmapper

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ba.unsa.etf.rpr.chatapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires bcrypt;
+    requires com.fasterxml.jackson.databind;
 
 
     opens ba.unsa.etf.rpr.chatapp to javafx.fxml;
