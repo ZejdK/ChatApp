4ba468613baa2bd95bcc39a589880770d99bc82d
Zejd Krek <zejd.krek@gmail.com>
2023-12-17 18:19:08 +0100

    add a client config data access object which reads user configured file of the server address

---
+++ src/main/java/ba/unsa/etf/rpr/chatapp/ClientConfigDao.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.chatapp;
+
+public final class ClientConfigDao {
+
+    private String serverUrl;
+    private int serverPort;
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+}
