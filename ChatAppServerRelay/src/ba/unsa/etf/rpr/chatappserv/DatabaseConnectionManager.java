06a86f4a44225cc299934e823af1756fdb2f22d8
Zejd Krek <zejd.krek@gmail.com>
2023-12-17 18:38:32 +0100

    add user lookup method to DatabaseConnectionManager class

--- ChatAppServerRelay/src/ba/unsa/etf/rpr/chatappserv/DatabaseConnectionManager.java
+++ ChatAppServerRelay/src/ba/unsa/etf/rpr/chatappserv/DatabaseConnectionManager.java
@@ -19,4 +19,24 @@ public class DatabaseConnectionManager {
         PreparedStatement ps = databaseConnection.prepareStatement(query);
         return ps.executeQuery();
     }
+
+    public UserDao LookupUser(String username) throws SQLException {
+
+        int rowCount = 0;
+        UserDao user = null;
+        ResultSet rs = this.runQuery(String.format("SELECT * FROM user WHERE username = '%s'", username));
+
+        while (rs.next()) {
+
+            user = new UserDao(rs.getLong("id"), rs.getString("username"), rs.getString("passwordhash"));
+            System.out.printf("id %d username %s password %s\n", user.getDatabaseId(), user.getUsername(), user.getPasswordHash());
+            ++rowCount;
+        }
+
+        if (rowCount > 1)
+            System.out.println("WARNING: Multiple users with the same username have popped up!");
+        System.out.println("Read " + rowCount + " rows from the database");
+
+        return user;
+    }
 }
