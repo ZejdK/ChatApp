0de1f15212a111f3257ef5808d419641395b3067
Zejd Krek <zejd.krek@gmail.com>
2023-12-17 18:25:47 +0100

    read connection data from a file when the program runs and add database connection logic

--- ChatAppServerRelay/src/ba/unsa/etf/rpr/chatappserv/Main.java
+++ ChatAppServerRelay/src/ba/unsa/etf/rpr/chatappserv/Main.java
@@ -1,17 +1,49 @@
 package ba.unsa.etf.rpr.chatappserv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int configurationPort = 30120;
-        // todo: citati konfiguraciju iz fajla
+        System.out.println("Logging onto the database...");
+        ServerConfigDao servConfig = LoadServerConfiguration();
+        DatabaseConnectionManager dbm = ConnectToDatabase(servConfig);
+        System.out.println("Successfully connected to the database!");
+
+        System.out.println("Starting connection manager...");
+        try {
+            ConnectionManager cm = new ConnectionManager(dbm);
+            cm.startServer(servConfig.getServerPort());
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    private static ServerConfigDao LoadServerConfiguration() throws RuntimeException {
 
         try {
+            String filepath = "C:\\Users\\Zejd\\IdeaProjects\\RPR\\Projekt\\ChatApp\\ChatAppServerRelay\\resources\\configserv.json";
+            return (new ObjectMapper()).readValue(new File(filepath), ServerConfigDao.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            String errorMsg = "Unable to start the server: There was an error loading the configuration file!";
+            System.out.println(errorMsg);
+            System.exit(0);
+            throw new RuntimeException(errorMsg);
+        }
+    }
 
-            ConnectionManager cm = new ConnectionManager();
-            cm.startServer(configurationPort);
+    private static DatabaseConnectionManager ConnectToDatabase(ServerConfigDao servConfig) throws RuntimeException {
 
-        } catch (Exception e) { e.printStackTrace(); }
+        try {
+            return new DatabaseConnectionManager(servConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+            String errorMsg = "Unable to start the server: There was an error connecting to the database!";
+            System.out.println(errorMsg);
+            System.exit(0);
+            throw new RuntimeException(errorMsg);
+        }
     }
 }
