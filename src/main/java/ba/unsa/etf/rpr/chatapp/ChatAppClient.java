17f141cf2f44063ce9abe9b4b9f580fbaaaa2420
Zejd Krek <zejd.krek@gmail.com>
2023-12-17 18:48:15 +0100

    refactor to rename HelloApplication to a more appropriate name ChatAppClient

--- src/main/java/ba/unsa/etf/rpr/chatapp/ChatAppClient.java
+++ src/main/java/ba/unsa/etf/rpr/chatapp/ChatAppClient.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
-public class HelloApplication extends Application {
+public class ChatAppClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginWindow.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatAppClient.class.getResource("loginWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
         stage.setTitle("Log in");
         stage.setScene(scene);
