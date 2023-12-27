3444a49554e388399f7272f786b855fe7e475e1d
Zejd Krek <zejd.krek@gmail.com>
2023-12-27 12:30:24 +0100

    add a method to run insert queries into the DatabaseConnection class

--- src/main/java/ba/unsa/etf/rpr/chatappserver/DatabaseConnection.java
+++ src/main/java/ba/unsa/etf/rpr/chatappserver/DatabaseConnection.java
@@ -42,5 +42,23 @@ public class DatabaseConnection {
         return ps.executeQuery();
     }
 
+    public ResultSet runInsertQuery(String statement, Object[] params) throws SQLException {
 
+        PreparedStatement q = databaseConnection.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS);
+
+        int counter = 1;
+        for (Object o : params) {
+
+            if (o instanceof String str)
+                q.setString(counter, str);
+            else if (o instanceof Integer n)
+                q.setInt(counter, n);
+
+            ++counter;
+        }
+
+        q.executeUpdate();
+
+        return q.getGeneratedKeys();
+    }
 }
