51d22a8b86b55c2607c3bbaf230f17922d9a1209
Zejd Krek <zejd.krek@gmail.com>
2023-12-27 12:29:43 +0100

    remove code for creating and finding a user and from DatabaseConnection class

--- src/main/java/ba/unsa/etf/rpr/chatappserver/DatabaseConnection.java
+++ src/main/java/ba/unsa/etf/rpr/chatappserver/DatabaseConnection.java
@@ -1,10 +1,5 @@
 package ba.unsa.etf.rpr.chatappserver;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
-import ba.unsa.etf.rpr.LoginData;
-import ba.unsa.etf.rpr.User;
-
-import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
@@ -47,47 +42,5 @@ public class DatabaseConnection {
         return ps.executeQuery();
     }
 
-    public User Lookup(String username) throws SQLException {
 
-        int rowCount = 0;
-        User user = null;
-        ResultSet rs = this.runQuery(String.format("SELECT * FROM users WHERE username = '%s'", username));
-
-        while (rs.next()) {
-
-            user = new User(rs.getLong("id"), rs.getString("username"), rs.getString("passwordhash"));
-            System.out.printf("id %d username %s password %s\n", user.getDatabaseId(), user.getUsername(), user.getPasswordHash());
-            ++rowCount;
-        }
-
-        if (rowCount > 1)
-            System.out.println("WARNING: Multiple users with the same username returned from the database!");
-        System.out.println("Read " + rowCount + " rows from the database");
-
-        return user;
-    }
-
-    public User CreateUser(LoginData loginData) {
-
-        try {
-            PreparedStatement userInsertQuery = databaseConnection.prepareStatement("INSERT INTO users (username, passwordhash) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-            userInsertQuery.setString(1, loginData.username);
-            userInsertQuery.setString(2, BCrypt.withDefaults().hashToString(12, loginData.password.toCharArray()));
-
-            userInsertQuery.executeUpdate();
-
-            ResultSet keys = userInsertQuery.getGeneratedKeys();
-
-            System.out.println("Fetch size: " + keys.getFetchSize());
-
-            while (keys.next()) {
-                System.out.println(keys);
-            }
-
-            return Lookup(loginData.username);
-
-        } catch (Exception e) { e.printStackTrace(); System.out.println("Error creating user!"); }
-
-        return null;
-    }
 }
