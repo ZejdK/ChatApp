2848f91e32993c4209e4f4c9bb40c1cc2dcfc1e6
Zejd Krek <zejd.krek@gmail.com>
2024-01-11 01:19:29 +0100

    User class implements Serializable

--- src/main/java/ba/unsa/etf/rpr/chatapp/server/beans/User.java
+++ src/main/java/ba/unsa/etf/rpr/chatapp/server/beans/User.java
@@ -1,8 +1,9 @@
 package ba.unsa.etf.rpr.chatapp.server.beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class User {
+public class User implements Serializable {
 
     private long id;
     private String username;
